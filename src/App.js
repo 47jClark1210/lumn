2f0317b6f817d66498e8bd166f205633ff76eb04
47jClark1210 <jclark@icstars.org>
2025-07-08 15:46:56 -0500

    Add routing and layout structure to the App component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import 'antd/dist/antd.css';
-import logo from './Solar Eclipse Favicon.ico';
 import './App.css';
+import { Layout, Menu } from 'antd';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from 'react-router-dom';
+
+const { Header, Content, Footer, Sider } = Layout;
+
+function Analytics() {
+  return <h2>Dashboard</h2>;
+}
+function OKRs() {
+  return <h2>OKRs</h2>;
+}
+function Collaboration() {
+  return <h2>Collaboration</h2>;
+}
+function Reporting() {
+  return <h2>Reporting</h2>;
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Layout style={{ minHeight: '100vh' }}>
+        <Sider breakpoint="lg" collapsedWidth="0">
+          <div className="logo" style={{ color: 'white', textAlign: 'center', padding: 16, fontWeight: 'bold' }}>
+            lumn | 1898 & Co.
+          </div>
+          <Menu theme="dark" mode="inline" defaultSelectedKeys={['dashboard']}>
+            <Menu.Item key="analytics">
+              <Link to="/">Analytics</Link>
+            </Menu.Item>
+            <Menu.Item key="okrs">
+              <Link to="/okrs">OKRs</Link>
+            </Menu.Item>
+            <Menu.Item key="collaboration">
+              <Link to="/collaboration">Collaboration</Link>
+            </Menu.Item>
+            <Menu.Item key="reporting">
+              <Link to="/reporting">Reporting</Link>
+            </Menu.Item>
+          </Menu>
+        </Sider>
+        <Layout>
+          <Header style={{ background: '#fff', padding: 0, textAlign: 'center', fontWeight: 'bold' }}>
+            Team OKR Tracking
+          </Header>
+          <Content style={{ margin: '24px 16px 0' }}>
+            <div style={{ padding: 24, background: '#fff', minHeight: 360 }}>
+              <Routes>
+                <Route path="/" element={<Analytics />} />
+                <Route path="/okrs" element={<OKRs />} />
+                <Route path="/teams" element={<Collaboration />} />
+                <Route path="/reports" element={<Reporting />} />
+              </Routes>
+            </div>
+          </Content>
+          <Footer style={{ textAlign: 'center' }}>
+            lumn ©2025 Created by Ethereal Strategies in conjunction with Ant Design
+          </Footer>
+        </Layout>
+      </Layout>
+    </Router>
   );
 }
 
